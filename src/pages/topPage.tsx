df4e3c0d5fadf27448fcad0ab3f3c65ea6b747a5
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-07-07 00:38:57 +0900

    toppage

---
+++ src/pages/topPage.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import TopHeader from '../components/topPage/topHeader';
+import TopMain from '../components/topPage/topMain';
+
+const TopPage: FC = () => {
+    return (
+        <div>
+            <TopHeader />
+            <TopMain />
+        </div>
+    )
+}
+
+export default TopPage;
