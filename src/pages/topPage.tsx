497343a3644b8d5f326d2aca80bcfb7f817ed460
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-07-08 15:04:21 +0900

    switch router

--- src/pages/topPage.tsx
+++ src/pages/topPage.tsx
@@ -1,13 +1,26 @@
 import React, {FC} from 'react';
+import DownloadPage from './downloadPage';
 import TopHeader from '../components/topPage/topHeader';
 import TopMain from '../components/topPage/topMain';
+import ResultPage from './resultPage';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const TopPage: FC = () => {
     return (
-        <div>
-            <TopHeader />
-            <TopMain />
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <TopHeader />
+                    <TopMain />
+                </Route>
+                <Route path='/search/:keyword' exact>
+                    <ResultPage />
+                </Route>
+                <Route path='/download/:keyword' exact>
+                    <DownloadPage />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
